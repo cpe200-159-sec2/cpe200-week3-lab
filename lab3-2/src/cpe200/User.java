b6cb7218bc85836abca3720b7963cc3fc5dd5bd4
kabutoboy <kabutoboy@gmail.com>
2016-09-09 16:13:19 +0700

    Lab 2 done

--- lab3-2/src/cpe200/User.java
+++ lab3-2/src/cpe200/User.java
@@ -1,38 +1,54 @@
 package cpe200;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    protected String userName;
+    protected final String USERNAME_PATTERN = "[A-Za-z][A-Za-z0-9]{7,}";
+    protected final String PASSWORD_PATTERN = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{12}";
+
+    protected String username;
     protected String password;
+
+    protected Pattern usernamePattern;
+    protected Pattern passwordPattern;
 
     public User()
     {
-        /* your code here */
+        usernamePattern = Pattern.compile(USERNAME_PATTERN);
+        passwordPattern = Pattern.compile(PASSWORD_PATTERN);
     }
 
-    public boolean setUserName(String name)
+    public boolean setUserName(String username)
     {
-        /* your code here */
+        Matcher m = usernamePattern.matcher(username);
+        if (m.find()) {
+            this.username = username;
+            return true;
+        }
         return false;
     }
 
-    public boolean setPassword(String name)
+    public boolean setPassword(String password)
     {
-        /* your code here */
+        Matcher m = passwordPattern.matcher(password);
+        if (m.find()) {
+            this.password = password;
+            return true;
+        }
         return false;
     }
 
     public String getUserName()
     {
-        /* your code here */
-        return null;
+        return username;
     }
 
     public String getPassword()
     {
-        /* your code here */
-        return null;
+        return password;
     }
 }
