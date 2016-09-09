c9c8f9b949d41b31e92d3bf394950d30e334afdd
kabutoboy <kabutoboy@gmail.com>
2016-09-09 15:25:47 +0700

    Lab 1 done

--- lab3_1/src/cpe200/BinaryCalculator.java
+++ lab3_1/src/cpe200/BinaryCalculator.java
@@ -1,51 +1,63 @@
 package cpe200;
 
+import java.lang.*;
+
 /**
  * Created by pruet on 5/9/2559.
  */
 
 public class BinaryCalculator {
-    /* your code here */
+    double val1;
+    double val2;
 
     public BinaryCalculator()
     {
-        /* your code here */
+        val1 = val2 = 0.0d;
     }
 
     public void setFirstOperand(Operand operand)
     {
-        /* your code here */
+        val1 = Double.parseDouble(operand.operand);
     }
 
 
     public void setSecondOperand(Operand operand)
     {
-        /* your code here */
+        val2 = Double.parseDouble(operand.operand);
+    }
+
+    protected String format(double val) {
+        if (Math.abs(1e5 * val - 1e5 * Math.round(val)) < 1) {
+            return String.format("%d", (long)val);
+        } else {
+            return String.format("%s", (double) Math.round(1e5 * val) / 100000);
+        }
     }
 
     public String add()
     {
-        /* your code here */
-        return null;
+        double result = val1 + val2;
+        return format(result);
     }
 
     public String subtract()
     {
-        /* your code here */
-        return null;
+        double result = val1 - val2;
+        return format(result);
     }
 
     public String multiply()
     {
-        /* your code here */
-        return null;
+        double result = val1 * val2;
+        return format(result);
     }
 
     /* This method should throw an exception when divide by zero */
     public String division()
     {
-        /* your code here */
-        return null;
+        if (Math.abs(val2) < 1e-5) throw new RuntimeException("Cannot divide by zero");
+        double result = val1 / val2;
+        return format(result);
     }
 
 
