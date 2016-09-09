5c2751a7d9ecd0dcc07a3a6883b56c8866328ec5
ASUS_kwpinthong <kw.pinthong@gmail.com>
2016-09-09 16:17:44 +0700

    UnComplete_LAB1

--- lab3_1/src/cpe200/BinaryCalculator.java
+++ lab3_1/src/cpe200/BinaryCalculator.java
@@ -1,11 +1,15 @@
 package cpe200;
 
+import java.util.DoubleSummaryStatistics;
+
 /**
  * Created by pruet on 5/9/2559.
  */
 
 public class BinaryCalculator {
     /* your code here */
+    private Operand firstOperand;
+    private Operand secondOperand;
 
     public BinaryCalculator()
     {
@@ -15,37 +19,59 @@ public class BinaryCalculator {
     public void setFirstOperand(Operand operand)
     {
         /* your code here */
+       firstOperand = operand;
     }
 
 
     public void setSecondOperand(Operand operand)
     {
         /* your code here */
+        secondOperand = operand;
+
     }
 
     public String add()
     {
         /* your code here */
-        return null;
+        if(firstOperand.equals(".")||secondOperand.equals(".")){
+            return Double.toString(Double.valueOf(firstOperand.operand)+Double.valueOf(secondOperand.operand));
+        }else{
+            return Integer.toString(Integer.valueOf(firstOperand.operand)+Integer.valueOf(secondOperand.operand));
+        }
+
     }
 
     public String subtract()
     {
         /* your code here */
-        return null;
+        if(firstOperand.equals(".")||secondOperand.equals(".")){
+            return Double.toString(Double.valueOf(firstOperand.operand)-Double.valueOf(secondOperand.operand));
+        }else{
+            return Integer.toString(Integer.valueOf(firstOperand.operand)-Integer.valueOf(secondOperand.operand));
+        }
+
     }
 
     public String multiply()
     {
         /* your code here */
-        return null;
+        if(firstOperand.equals(".")||secondOperand.equals(".")){
+            //double sum = Double.toString(Double.valueOf(firstOperand.operand))
+            return Double.toString(Double.valueOf(firstOperand.operand)*Double.valueOf(secondOperand.operand));
+        }else{
+            return Integer.toString(Integer.valueOf(firstOperand.operand)*Integer.valueOf(secondOperand.operand));
+        }
+
     }
 
     /* This method should throw an exception when divide by zero */
-    public String division()
-    {
+    public String division() {
         /* your code here */
-        return null;
+        if(firstOperand.equals(".")||secondOperand.equals(".")){
+            return Double.toString(Double.valueOf(firstOperand.operand)/Double.valueOf(secondOperand.operand));
+        }else{
+            return Integer.toString(Integer.valueOf(firstOperand.operand)/Integer.valueOf(secondOperand.operand));
+        }
     }
 
 
