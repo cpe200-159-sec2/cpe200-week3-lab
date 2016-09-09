812c1b64dbbb77157c44d24f338c3c4c4ef60db8
nohajime <patchara0pt002@gmail.com>
2016-09-09 17:01:43 +0700

    lab 1 week 3

--- lab3_1/src/cpe200/BinaryCalculator.java
+++ lab3_1/src/cpe200/BinaryCalculator.java
@@ -1,51 +1,78 @@
 package cpe200;
 
+import java.math.BigDecimal;
+
 /**
  * Created by pruet on 5/9/2559.
  */
 
 public class BinaryCalculator {
     /* your code here */
+    private Operand firstOperand;
+    private Operand secondOperand;
+    private Double first;
+    private Double second;
+
 
     public BinaryCalculator()
     {
-        /* your code here */
+        firstOperand = new Operand('0');
+        secondOperand = new Operand('0');
     }
 
     public void setFirstOperand(Operand operand)
     {
-        /* your code here */
+        this.firstOperand = operand;
     }
 
 
     public void setSecondOperand(Operand operand)
     {
-        /* your code here */
+        secondOperand = operand;
     }
 
     public String add()
     {
-        /* your code here */
-        return null;
+        first = Double.parseDouble(firstOperand.operand);
+        second = Double.parseDouble(secondOperand.operand);
+        String sum = new Double(first+second).toString();
+
+        return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$","").replaceAll("\\.$","");
     }
 
     public String subtract()
     {
-        /* your code here */
-        return null;
+        first = Double.parseDouble(firstOperand.operand);
+        second = Double.parseDouble(secondOperand.operand);
+        Double sumdb = first-second;
+        String sum = new Double(first-second).toString();
+        sum = String.format("%.5f",sumdb);
+
+        return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$","").replaceAll("\\.$","");
     }
 
     public String multiply()
     {
-        /* your code here */
-        return null;
+        first = Double.parseDouble(firstOperand.operand);
+        second = Double.parseDouble(secondOperand.operand);
+        String sum = new Double(first*second).toString();
+
+        return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$","").replaceAll("\\.$","");
+
     }
 
     /* This method should throw an exception when divide by zero */
     public String division()
     {
-        /* your code here */
-        return null;
+        first = Double.parseDouble(firstOperand.operand);
+        second = Double.parseDouble(secondOperand.operand);
+        if(second==0) throw new RuntimeException("ERROR");
+        Double sumdb = first/second;
+
+        String sum = new Double(first/second).toString();
+        sum = String.format("%.5f",sumdb);
+        return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$","").replaceAll("\\.$","");
+
     }
 
 
