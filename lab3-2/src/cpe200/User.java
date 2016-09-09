a71bd7a5df96b5ef40cdb32efcf69d91b177719a
nohajime <patchara0pt002@gmail.com>
2016-09-09 17:11:25 +0700

    lab 2 week 3

--- lab3-2/src/cpe200/User.java
+++ lab3-2/src/cpe200/User.java
@@ -9,30 +9,36 @@ public class User {
 
     public User()
     {
-        /* your code here */
+        userName = new String();
+        password = new String();
     }
 
     public boolean setUserName(String name)
     {
-        /* your code here */
-        return false;
+        String temp = "[A-Za-z][A-Za-z0-9]{7,}";
+        if(name.matches(temp)){
+            userName = name;
+            return true;
+        }else return false;
     }
 
     public boolean setPassword(String name)
     {
-        /* your code here */
-        return false;
+        String temp = "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-1])[A-Za-z0-9]{12,}";
+        if(name.matches(temp)){
+            password = name;
+            return true;
+        }else return false;
+
     }
 
     public String getUserName()
     {
-        /* your code here */
-        return null;
+        return userName;
     }
 
     public String getPassword()
     {
-        /* your code here */
-        return null;
+        return password;
     }
 }
