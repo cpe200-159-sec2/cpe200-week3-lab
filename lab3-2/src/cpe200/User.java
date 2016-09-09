e0179647a25ff8607e64378fb15558192f2506c5
OomDuanghong <d.nuttinee@hotmail.com>
2016-09-09 17:56:11 +0700

    Lab Week3 : Finished

--- lab3-2/src/cpe200/User.java
+++ lab3-2/src/cpe200/User.java
@@ -7,32 +7,32 @@ public class User {
     protected String userName;
     protected String password;
 
-    public User()
-    {
-        /* your code here */
+    public User() {
+
+    }
+    public boolean setUserName(String name){
+        String Ncheck = "^([A-za-z])([A-Za-z0-9]{7,})$";
+        if (name.matches(Ncheck)){
+            userName = name;
+            return true;
+        }
+        else
+            return false;
+    }
+    public boolean setPassword(String name){
+        String PCheck = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])([A-Za-z0-9]{12,})$";
+        if (name.matches(PCheck)){
+            password = name;
+            return true;
+        }
+        else
+            return false;
+    }
+    public String getUserName(){
+        return userName;
     }
 
-    public boolean setUserName(String name)
-    {
-        /* your code here */
-        return false;
-    }
-
-    public boolean setPassword(String name)
-    {
-        /* your code here */
-        return false;
-    }
-
-    public String getUserName()
-    {
-        /* your code here */
-        return null;
-    }
-
-    public String getPassword()
-    {
-        /* your code here */
-        return null;
+    public String getPassword(){
+        return password;
     }
 }
--- lab3_1/src/cpe200/BinaryCalculator.java
+++ lab3_1/src/cpe200/BinaryCalculator.java
@@ -1,52 +1,47 @@
 package cpe200;
 
+import java.math.BigDecimal;
+
 /**
  * Created by pruet on 5/9/2559.
  */
 
 public class BinaryCalculator {
-    /* your code here */
+    BigDecimal first, second;
 
-    public BinaryCalculator()
-    {
-        /* your code here */
+    public BinaryCalculator() {
+
     }
 
-    public void setFirstOperand(Operand operand)
-    {
-        /* your code here */
-    }
-
-
-    public void setSecondOperand(Operand operand)
-    {
-        /* your code here */
-    }
-
-    public String add()
-    {
-        /* your code here */
-        return null;
-    }
-
-    public String subtract()
-    {
-        /* your code here */
-        return null;
-    }
-
-    public String multiply()
-    {
-        /* your code here */
-        return null;
-    }
-
-    /* This method should throw an exception when divide by zero */
-    public String division()
-    {
-        /* your code here */
-        return null;
+    public void setFirstOperand(Operand operand) {
+        first = new BigDecimal(operand.operand);
     }
 
 
+    public void setSecondOperand(Operand operand) {
+        second = new BigDecimal(operand.operand);
+    }
+
+    public String add() {
+        first = first.add(second).stripTrailingZeros();
+        return first.toString();
+    }
+
+    public String subtract() {
+        first = first.subtract(second).stripTrailingZeros();
+        return first.toString();
+    }
+
+    public String multiply() {
+        first = first.multiply(second).stripTrailingZeros();
+        return first.toString();
+    }
+
+    public String division() {
+        if (second.toString().equals("0")) {
+            throw new RuntimeException();
+        } else
+            first = first.divide(second, 5, BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
+        return first.toString();
+    }
 }
