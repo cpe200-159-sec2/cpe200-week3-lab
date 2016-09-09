f765a0fab1de25ee52c4fb215966b00c765179e6
cwm <warakornchamp@gmail.com>
2016-09-09 16:25:48 +0700

    finish lab 3-2

--- lab3-2/src/cpe200/User.java
+++ lab3-2/src/cpe200/User.java
@@ -1,5 +1,7 @@
 package cpe200;
 
+import java.util.regex.Matcher;
+
 /**
  * Created by pruet on 6/9/2559.
  */
@@ -7,32 +9,34 @@ public class User {
     protected String userName;
     protected String password;
 
-    public User()
-    {
+
+    public User() {
         /* your code here */
     }
 
-    public boolean setUserName(String name)
-    {
-        /* your code here */
-        return false;
+    public boolean setUserName(String name) {
+        String check = "^[A-Za-z][A-Za-z0-9]{7,}$";
+        if(name.matches(check)) {
+            userName=name;
+            return true;
+        }
+        else return false;
     }
 
-    public boolean setPassword(String name)
-    {
-        /* your code here */
-        return false;
+    public boolean setPassword(String name) {
+        String check = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{12,}$";
+        if(name.matches(check)){
+            password=name;
+            return true;
+        }
+        else return false;
     }
 
-    public String getUserName()
-    {
-        /* your code here */
-        return null;
+    public String getUserName() {
+        return userName;
     }
 
-    public String getPassword()
-    {
-        /* your code here */
-        return null;
+    public String getPassword() {
+        return password;
     }
 }
